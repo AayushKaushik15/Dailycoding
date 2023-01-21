e751215ddcbb6812a8a023dfb66c394cda8e6e22
Ayush Kaushik <73561218+Ayush1557@users.noreply.github.com>
2023-01-21 15:38:52 +0530

    new changes

--- Inheritance/Show.java
+++ Inheritance/Show.java
@@ -11,5 +11,18 @@ public class Show {
 		obj2.display();
 		Child.scream();
 		System.out.println(obj2.name +  " " + obj2.number + " " + obj2.naam);
+		
+		
+		Main ob3 = new Child(123, "ayush", "shashank");
+        System.out.println(ob3.name + " " + ob3.number);
+
+        //NOTE THAT
+//        there are many variables in both parent and child classes
+        // you are given access to variables that are in the ref type i.e. child class
+        // hence, you should have access to naam variable
+        // this also means, that the ones you are trying to access should be initilized
+        //but here, when the object itself if of type parent class, how will you call the child class variables
+        //this is why error
+//        Child ob4 = new Main(23, "Ayush");
 	}
 }
