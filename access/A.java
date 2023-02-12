6a8ffaff8e36fdd11cf039320eff6c7a0c7e8059
Ayush <shashankkaushik106@gmail.com>
2023-02-12 12:10:04 +0530

    Access modifiers, Encapsulation

---
+++ access/A.java
@@ -0,0 +1,13 @@
+package DailyCoding.access;
+
+public class A {
+    int num;
+    String name;
+    int [] arr;
+
+    public A(int num, String name, int[] arr) {
+        this.num = num;
+        this.name = name;
+        this.arr = arr;
+    }
+}
